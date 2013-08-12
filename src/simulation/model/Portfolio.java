17e7c58af45f2bb8efb5a3f1c375e20d29be7aee
Pei Wang <steven_wang_pei@hotmail.com>
2013-08-12 15:33:10 +1000

    create portfolio class, which is expected to store derivatives

--- src/simulation/model/Portfolio.java
+++ src/simulation/model/Portfolio.java
@@ -1,6 +1,51 @@
 package simulation.model;
 
+import java.util.LinkedList;
+
+import view.SimulationView;
+
 public class Portfolio {
+
+	public LinkedList<Derivative> getPortfolio() {
+		return portfolio;
+	}
+
+	public void setPortfolio(LinkedList<Derivative> portfolio) {
+		this.portfolio = portfolio;
+	}
+
+	private void updateIndexTable(){
+		indextable = simulation.get_indextable();
+
+	}
+	
+	public float getUnderlyingPrice(String sym){
+		if(simulation.newtablecontent == true){
+			updateIndexTable();
+			simulation.newtablecontent = false;
+		}
+		float price = 0;
+		for(String[] row : indextable){
+			if(row[0].equals(sym)){
+				price = Float.parseFloat(row[1]);
+				break;
+			}
+		}
+		return price;
+	}
+	
+	private LinkedList<Derivative> portfolio;
+	private SimulationView simulation;
+	private String[][] indextable;
+	
+	public Portfolio(LinkedList<Derivative> portfolio, SimulationView sv) {
+		super();
+		this.portfolio = portfolio;
+		this.simulation = sv;
+	}
+
+
+
 
 	/**
 	 * @param args
