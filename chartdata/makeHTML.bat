006bd6b84db1468cd823f108490a7f812483c324
Pei Wang <steven_wang_pei@hotmail.com>
2013-08-24 01:59:00 +1000

    some change on amchart setting

--- chartdata/makeHTML.bat
+++ chartdata/makeHTML.bat
@@ -1,5 +1,10 @@
 @ echo off
-findstr /v "qwekljsdaiouc" header.txt >> mystockchart.html
+findstr /v "qwekljsdaiouc" header.txt >> mysetting.xml
+for /f "tokens=*" %%a in (header.txt) do (
+  echo line=%%a
+)
+echo ABT.csv  >> mysetting.xml
+findstr /v "qwekljsdaiouc" footer.txt >> mysetting.xml
 for /f "tokens=*" %%a in (header.txt) do (
   echo line=%%a
 )
