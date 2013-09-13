0ed97c07b54d8acd0d5dfd6c2af51d93ccf1602e
Pei Wang <steven_wang_pei@hotmail.com>
2013-09-13 15:23:15 +1000

    pass the test on all orders

--- src/test/OrderTest.java
+++ src/test/OrderTest.java
@@ -3,6 +3,7 @@ package test;
 import static org.junit.Assert.*;
 
 import order.LimitOrder;
+import order.MarketOrder;
 import order.Order;
 import order.StopLimitOrder;
 import order.StopOrder;
@@ -62,7 +63,7 @@ public class OrderTest {
 		p.checkOrders(new String[][] {{"ABC", "7.4", "0.5", "9.09", "100"}});
 		p.printAll();
 		
-	}*/
+	}
 
 	@Test
 	public void test4() {
@@ -86,12 +87,15 @@ public class OrderTest {
 		System.out.println("---------Test5--------");
 
 		Portfolio p = new Portfolio(1000);
-		Order o1 = new StopOrder(new Stock((float)7.0, 10, "ABC"), "Long", 7);
-		Order o2 = new StopOrder(new Stock((float)8.0, 10, "ABC"), "Long", 8);
+		Order o1 = new StopOrder(new Stock((float)7.0, 30, "ABC"), "Short", 7);
+		Order o2 = new StopOrder(new Stock((float)8.0, 10, "ABC"), "Short", 8);
 
 		p.makeOrder(o1);
 		p.makeOrder(o2);
+		p.test_purchase(new Stock((float)7.0, 20, "ABC"), 7);
+		p.test_purchase(new Stock((float)8.0, 20, "ABC"), 8);
 
+		
 		p.printAll();
 		p.checkOrders(new String[][] {{"ABC", "7.4", "0.5", "9.09", "100"}});
 		p.printAll();
@@ -103,14 +107,32 @@ public class OrderTest {
 	public void test6() {
 		System.out.println("---------Test6--------");
 		Portfolio p = new Portfolio(1000);
-		Order o1 = new StopLimitOrder(new Stock((float)7.0, 10, "ABC"), "Long", 6, 7);
-		Order o2 = new StopLimitOrder(new Stock((float)6.5, 10, "ABC"), "Long", 7, (float)6.5);
+		Order o1 = new StopLimitOrder(new Stock((float)7.0, 10, "ABC"), "Short", 6, 7);
+		Order o2 = new StopLimitOrder(new Stock((float)6.5, 10, "ABC"), "Short", 7, (float)6.5);
 
 		p.makeOrder(o1);
 		p.makeOrder(o2);
+		p.test_purchase(new Stock((float)8.0, 5, "ABC"), 8);
+		p.test_purchase(new Stock((float)8.0, 5, "ABC"), 8);
 
 		p.printAll();
 		p.checkOrders(new String[][] {{"ABC", "7.4", "0.5", "9.09", "100"}});
+		p.printAll();
+		
+	}*/
+	@Test
+	public void test7() {
+		System.out.println("---------Test7--------");
+		Portfolio p = new Portfolio(1000);
+		Order o2 = new MarketOrder(new Stock((float)3.5, 10, "ABC"), "Short");
+		//Order o1 = new MarketOrder(new Stock((float)3.0, 10, "ABC"), "Long");
+
+		//p.makeOrder(o1);
+		p.makeOrder(o2);
+		p.test_purchase(new Stock((float)8.0, 5, "ABC"), 8);
+
+		p.printAll();
+		p.checkOrders(new String[][] {{"ABC", "6", "0.5", "9.09", "100"}});
 		p.printAll();
 		
 	}
